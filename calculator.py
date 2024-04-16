518f11a12c6367d49ae800a53979702d69720806
rupacesigdel <rupeshcgdl2060@gmail.com>
2024-04-17 00:13:45 +0545

    learning

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
 # hey rupesh,plese dont remove this line.
-print("hey i am a good boy\nand this viewer is also a good boy/girl")
+print("hey i am\' a \"good boy\"\nand this viewer is also a good boy/girl")
 
 print("helloword")
+print("hey", 6, 7,sep="~", end="009\n")
+print("rupesh")
