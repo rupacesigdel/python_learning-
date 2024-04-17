afae412619596836cbe40545a2261df38f240d28
rupacesigdel <rupeshcgdl2060@gmail.com>
2024-04-17 10:25:24 +0545

    py learning

---
+++ random_1.py
@@ -0,0 +1,56 @@
+# hey rupesh,plese dont remove this line.
+print("hey i am\' a \"good boy\"\nand this viewer is also a good boy/girl")
+'''
+print("helloword")
+'''
+print("hey", 6, 7,sep="~", end="009\n")
+print("rupesh")
+
+a=122444545873653264
+print(a)
+b="rupesh"
+print(b)
+
+print("types", type(a))
+print("types", type(b))
+
+#dictionnary
+
+dict1 ={"name":"rupesh","age":20,"canvote":True}
+print(dict1)
+
+#list
+
+integrates = ["egges", "flour", "sugar", "salt"]
+print(integrates[0:2])
+
+letters = ["A", "B", "C","D", "E"]
+print(letters[4:])
+print(letters[1:])
+print(letters[-2:])
+print(letters[2:4])
+print(letters[:2])
+print(letters[3:0])
+
+#operators 
+print(15+12)
+print(15-7)
+print(15/6)
+print(15*7)
+print(2**5)
+
+# ex
+n =  20
+m =  7
+ans1 =n+m 
+print("Addition of ",n, "and" ,m,"is",ans1)
+ans2 = n-m 
+print("subtraction of ",n, "and",m, "is", ans2)
+ans3 = n*m
+print("Multiplication of ",n, "and",m, "is", ans3)
+ans4 = n/m
+print("Division of ",n, "and",m, "is", ans4)
+ans5 = n%m
+print("Modulus of ",n, "and",m, "is", ans5)
+ans6 = n//m
+print("Floor division of ",n, "and",m, "is", ans6)
