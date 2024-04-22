64be280eeaf03289e18c2f51cd87349d5ec33e00
rupacesigdel <rupeshcgdl2060@gmail.com>
2024-04-22 19:07:29 +0545

    Enumerate_function

---
+++ Enumerate_function.py
@@ -0,0 +1,12 @@
+marks = [12, 56, 32, 98, 12, 45, 1, 4]
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if (index == 3):
+#         print("Rupesh sigdel")
+#     index +=1
+
+for index, mark in enumerate(marks, start=1):
+    print(mark)
+    if (index == 3):
+        print("Rupesh sigdel")
