57fc92575d7f42e7ad8db273445639a116403209
rupacesigdel <rupeshcgdl2060@gmail.com>
2024-05-02 00:28:05 +0545

    method overriding in python

---
+++ Overriding_method.py
@@ -0,0 +1,24 @@
+class shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def area(self):
+        return self.x * self.y
+    
+class Circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
+        super().__init__(radius, radius)
+
+    def area(self):
+        return 3.14 * super().area()
+    
+    # def area(self):
+    #     return 3.14 * self.radius * self.radius
+    
+    
+rec = shape(5, 3)
+print(rec.area())
+c = Circle(5)
+print(c.area())
